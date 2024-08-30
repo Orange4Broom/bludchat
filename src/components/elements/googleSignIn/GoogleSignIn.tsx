5d6507f21a542587e1338b17dcb35cd2281878fc
Orange4Broom <orang4broom@gmail.com>
2024-08-31 01:38:34 +0200

    Add aos library and use it GoogleSignIn

--- src/components/elements/googleSignIn/GoogleSignIn.tsx
+++ src/components/elements/googleSignIn/GoogleSignIn.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useGoogleSignIn } from "@hooks/useGoogleSignIn";
 import { Icon } from "@elements/icon/Icon";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export const GoogleSignIn: React.FC = () => {
   const { signInWithGoogle, loading } = useGoogleSignIn();
 
+  useEffect(() => {
+    AOS.init({ duration: 1200 });
+  }, []);
+
   return (
     <div className="heropage">
-      <div className="heropage__top">
+      <div className="heropage__top" data-aos="fade-down">
         <h1 className="heropage__header">Welcome to BludChat 👋</h1>
-        <p className="heropage__paragraph">My first chatting web aplication</p>
+        <p className="heropage__paragraph">My first chatting web application</p>
       </div>
-      <div className="heropage__line"></div>
-      <div className="heropage__mid">
+      <div className="heropage__line" data-aos="fade-up"></div>
+      <div className="heropage__mid" data-aos="fade-right">
         <h1 className="heropage__signin__header">Sign In / Sign Up</h1>
         <button
           className="heropage__signin__button"
@@ -23,7 +29,7 @@ export const GoogleSignIn: React.FC = () => {
           {loading ? "Signing in..." : "Sign in with Google"}
         </button>
       </div>
-      <div className="heropage__bottom">
+      <div className="heropage__bottom" data-aos="fade-left">
         <h1 className="heropage__github__header">My GitHub account</h1>
         <button
           className="heropage__github__button"
