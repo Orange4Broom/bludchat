dd1a096734660e121917f53bd596c95a79adab8f
Orange4Broom <orang4broom@gmail.com>
2024-09-04 00:13:38 +0200

    Add logout button with icon to improve user experience

--- src/components/elements/logout/Logout.tsx
+++ src/components/elements/logout/Logout.tsx
@@ -2,6 +2,13 @@ import React from "react";
 
 import { LogoutProps } from "@typings/LogoutProps";
 
+import "./logout.scss";
+import { Icon } from "@/components/elements/icon/Icon";
+
 export const Logout: React.FC<LogoutProps> = ({ onLogout }) => {
-  return <button onClick={onLogout}>Logout</button>;
+  return (
+    <button className="logout" onClick={onLogout}>
+      <Icon name="arrow-right-from-bracket" type="fas" />
+    </button>
+  );
 };
