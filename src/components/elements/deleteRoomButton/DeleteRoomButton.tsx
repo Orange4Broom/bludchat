a75dc5772abb0b47060d28e665abb1a0bd375fb9
Orange4Broom <orang4broom@gmail.com>
2024-09-03 23:19:18 +0200

    Update DeleteRoomButton

--- src/components/elements/deleteRoomButton/DeleteRoomButton.tsx
+++ src/components/elements/deleteRoomButton/DeleteRoomButton.tsx
@@ -3,12 +3,19 @@ import { useDeleteRoom } from "@hooks/room/useDeleteRoom";
 
 import { ChatRoomProps } from "@typings/ChatRoomProps";
 
+import "./deleteButton.scss";
+import { Icon } from "@/components/elements/icon/Icon";
+
 export const DeleteRoomButton: React.FC<ChatRoomProps> = ({ roomId }) => {
   const { deleteRoom, loading } = useDeleteRoom();
 
   return (
-    <button onClick={() => deleteRoom(roomId)} disabled={loading}>
-      {loading ? "Deleting..." : "Delete Room"}
+    <button
+      className="delete"
+      onClick={() => deleteRoom(roomId)}
+      disabled={loading}
+    >
+      {loading ? "Deleting..." : <Icon name="trash-can" type="fas" />}
     </button>
   );
 };
