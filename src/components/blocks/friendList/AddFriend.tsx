7670a03620954ee87f858ac4138e4c0aac05a9c4
Orange4Broom <orang4broom@gmail.com>
2024-09-07 02:05:16 +0200

    Refactor AddFriend component and add styling

--- src/components/blocks/friendList/AddFriend.tsx
+++ src/components/blocks/friendList/AddFriend.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { useAddFriend } from "@/hooks/friends/useAddFriend";
 
+import "./addFriends.scss";
+
 export const AddFriend: React.FC = () => {
-  const { friendId, setFriendId, addFriend } = useAddFriend();
+  const { friendId, setFriendId, addFriend, loading } = useAddFriend();
 
   return (
-    <div>
-      <h3>Add Friend</h3>
+    <form className="addfriend" onSubmit={addFriend}>
+      <h3 className="addfriend__header">Add Friend</h3>
       <input
+        className="addfriend__input"
         type="text"
         value={friendId}
         onChange={(e) => setFriendId(e.target.value)}
         placeholder="Enter Friend ID"
       />
-      <button onClick={addFriend}>Add Friend</button>
-    </div>
+      <button className="addfriend__submit" type="submit" disabled={loading}>
+        Add Friend
+      </button>
+    </form>
   );
 };
