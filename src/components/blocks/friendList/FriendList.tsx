6dfd3a225aa0359eeb367894f0a7408f2b737e51
Orange4Broom <orang4broom@gmail.com>
2024-09-07 02:04:52 +0200

    Refactor FriendList component and add styling

--- src/components/blocks/friendList/FriendList.tsx
+++ src/components/blocks/friendList/FriendList.tsx
@@ -16,6 +16,8 @@ import { FriendListProps } from "@typings/Friend";
 import { auth } from "@/firebase/firebase";
 import { Icon } from "@/components/elements/icon/Icon";
 
+import "./friendList.scss";
+
 export const FriendList: React.FC<FriendListProps> = ({
   uid,
   displayName,
@@ -69,65 +71,80 @@ export const FriendList: React.FC<FriendListProps> = ({
   };
 
   return (
-    <>
-      <h3>Friend List</h3>
-      <ul>
+    <div className="friendlist">
+      <h3 className="friendlist__header">Friend List</h3>
+      <div className="friendlist__list">
         {friends.map((friend) => (
-          <div key={friend.uid}>
-            <img
-              src={friend.photoURL}
-              alt="profilePicture"
-              style={{ height: "32px", width: "32px" }}
-            />
-            {friend.displayName}
-            {!handleFriendInRoom(friend.uid) &&
-            currentUser.uid === roomCreatorId ? (
-              <>
-                <abbr title="Add friend to room">
-                  <button onClick={() => handleAddToRoom(friend.uid)}>
-                    <Icon name="user-plus" type="fas" />
-                  </button>
-                </abbr>
-                <abbr title="Start chat with user">
-                  <button
-                    onClick={() => startChatWithFriend(friend)}
-                    disabled={chatLoading}
-                  >
-                    <Icon name="message" type="fas" />
-                  </button>
-                </abbr>
-                <abbr title="Remove user from friends">
-                  <button
-                    onClick={() => removeFriend(friend.uid, setFriends)}
-                    disabled={removeLoading}
-                  >
-                    <Icon name="user-minus" type="fas" />
-                  </button>
-                </abbr>
-              </>
-            ) : (
-              <>
-                <abbr title="Start chat with user">
-                  <button
-                    onClick={() => startChatWithFriend(friend)}
-                    disabled={chatLoading}
-                  >
-                    <Icon name="message" type="fas" />
-                  </button>
-                </abbr>
-                <abbr title="Remove user from Friends">
-                  <button
-                    onClick={() => removeFriend(friend.uid, setFriends)}
-                    disabled={removeLoading}
-                  >
-                    <Icon name="user-minus" type="fas" />
-                  </button>
-                </abbr>
-              </>
-            )}
+          <div key={friend.uid} className="friendlist__card">
+            <div className="friendlist__card__info">
+              <img
+                src={friend.photoURL}
+                alt="profilePicture"
+                style={{ height: "32px", width: "32px" }}
+              />
+              <p className="friendlist__card__info__username">{`${friend.displayName.substring(
+                0,
+                14
+              )}...`}</p>
+            </div>
+
+            <div className="friendlist__card__options">
+              {!handleFriendInRoom(friend.uid) &&
+              currentUser.uid === roomCreatorId ? (
+                <>
+                  <abbr title="Add friend to room">
+                    <button
+                      className="friendlist__card__option"
+                      onClick={() => handleAddToRoom(friend.uid)}
+                    >
+                      <Icon name="user-plus" type="fas" />
+                    </button>
+                  </abbr>
+                  <abbr title="Start chat with user">
+                    <button
+                      className="friendlist__card__option"
+                      onClick={() => startChatWithFriend(friend)}
+                      disabled={chatLoading}
+                    >
+                      <Icon name="message" type="fas" />
+                    </button>
+                  </abbr>
+                  <abbr title="Remove user from friends">
+                    <button
+                      className="friendlist__card__option"
+                      onClick={() => removeFriend(friend.uid, setFriends)}
+                      disabled={removeLoading}
+                    >
+                      <Icon name="user-minus" type="fas" />
+                    </button>
+                  </abbr>
+                </>
+              ) : (
+                <>
+                  <abbr title="Start chat with user">
+                    <button
+                      className="friendlist__card__option"
+                      onClick={() => startChatWithFriend(friend)}
+                      disabled={chatLoading}
+                    >
+                      <Icon name="message" type="fas" />
+                    </button>
+                  </abbr>
+                  <abbr title="Remove user from Friends">
+                    <button
+                      className="friendlist__card__option"
+                      onClick={() => removeFriend(friend.uid, setFriends)}
+                      disabled={removeLoading}
+                    >
+                      <Icon name="user-minus" type="fas" />
+                    </button>
+                  </abbr>
+                </>
+              )}
+            </div>
           </div>
         ))}
-      </ul>
-    </>
+      </div>
+    </div>
   );
 };
